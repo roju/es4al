e06e5dfb02138dc5c45383257734695833be37b2
metabenic <metabenic@gmail.com>
2023-11-21 10:02:44 -0800

    show default video on page load

--- assets/js/video-gallery.js
+++ assets/js/video-gallery.js
@@ -2,6 +2,12 @@ import { videoIds } from './constants.js'
 
 window.showGalleryVideo = showGalleryVideo;
 
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.location.pathname.endsWith('/video-gallery/')) {
+        showGalleryVideo([0,0]);
+    }
+});
+
 async function showGalleryVideo(idArrPosition) {
     const algorithm = idArrPosition[0];
     const learningMethod = idArrPosition[1];
